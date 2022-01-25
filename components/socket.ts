1186344cb16a1b91ba9f25f4b1b8a7f9207ca144
Kenneth Pham <kennethpham98@gmail.com>
2022-01-25 00:05:53 -0800

    Added react-native-dotenv and socket.io connection

---
+++ components/socket.ts
@@ -0,0 +1,25 @@
+import { io, Socket } from 'socket.io-client';
+import { SERVER_URL } from '@env';
+
+interface ServerToClientEvents { }
+interface ClientToServerEvents { }
+interface InterServerEvents { }
+interface SocketData { }
+
+const CURR_SERVER_URL = SERVER_URL;
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(CURR_SERVER_URL);
+
+socket.on('connect', () => {
+  console.log(`connected as ${socket.id}`);
+});
+
+export {
+  socket,
+  ServerToClientEvents,
+  ClientToServerEvents,
+  InterServerEvents,
+  SocketData
+};
+
+export default socket;
--- routes/Routes.tsx
+++ routes/Routes.tsx
@@ -1,18 +1,22 @@
-import { FC } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from '../screens/Login';
 
+import SocketProvider from '../components/SocketContext';
+
 const Stack = createNativeStackNavigator();
 
-const Routes: FC = () => {
+const Routes = () => {
   return (
+    <SocketProvider>
       <Stack.Navigator>
         <Stack.Screen
           name='Login'
           component={Login}
         />
       </Stack.Navigator>
+
+    </SocketProvider>
   );
 }
 
